d8c2258e0d123171cd48c57141f672a23beb2410
hanumathsai26-cmyk <hanumathsai26@gmail.com>
2026-02-01 22:34:43 +0530

    Delete Downloads/ExpenseSharingApplication/ExpenseSharingApplication directory

--- Downloads/ExpenseSharingApplication/ExpenseSharingApplication/src/main/java/com/jsp/ExpenseSharingApplication/REPOSITORY/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jsp.ExpenseSharingApplication.REPOSITORY;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jsp.ExpenseSharingApplication.DTO.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
--- Downloads/ExpenseSharingApplication/ExpenseSharingApplication/src/main/java/com/jsp/ExpenseSharingApplication/REPOSITORY1/ExpenseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jsp.ExpenseSharingApplication.REPOSITORY1;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jsp.ExpenseSharingApplication.DTO1.Expense;
-
-public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
-
-}

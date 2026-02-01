d484bcb0b730e9c8a9aa080effbb151aaf1678f4
vardh <vardh@192.168.1.9>
2026-02-01 22:20:41 +0530

    initial commit

---
+++ Downloads/lostandfoundthingswebservice/src/main/java/com/jsp/lostandfoundthingswebservice/LostandfoundthingswebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.lostandfoundthingswebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostandfoundthingswebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostandfoundthingswebserviceApplication.class, args);
+	}
+
+}
